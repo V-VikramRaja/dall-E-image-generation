0a653be25a4a481e73e7da3cb708b17c86d07b29
V-VikramRaja <vvraja2301@gmail.com>
2023-01-27 06:08:40 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -6,28 +6,25 @@ import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 
-// To read dotenv file
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-// connecting to api(s) from the files
-app.use("/api/v1//post", postRoutes);
+
+app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
-// Route
 app.get("/", async (req, res) => {
-  res.send("Hello from Dall-E");
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
 });
 
-// Connecting to the server
-const startServer = async (req, res) => {
+const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () =>
-      console.log("Server has started on post http://localhost:8080 ")
-    );
+    app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
