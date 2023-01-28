e535d6627d80e38b2c93e9430440a4bfefb35b05
V-VikramRaja <vvraja2301@gmail.com>
2023-01-28 11:45:57 +0530

    Setting the home page to view the images stored in the backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,6 +15,44 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPost, setAllPost] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/post", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        const result = await response.json();
+        setAllPost(result.data.reverse());
+      }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPost.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchResults(searchResults);
+      }, 500)
+    );
+  };
 
   return (
     <div>
@@ -30,7 +68,14 @@ const Home = () => {
         </div>
 
         <div className="mt-16">
-          <FormField />
+          <FormField
+            labelName="Search posts"
+            type="text"
+            name="text"
+            placeholder="Search Posts"
+            value={searchText}
+            handleChange={handleSearchChange}
+          />
         </div>
 
         <div className="mt-10">
@@ -48,9 +93,12 @@ const Home = () => {
               )}
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                 {searchText ? (
-                  <RenderCards data={[]} title="No search results found" />
+                  <RenderCards
+                    data={searchResults}
+                    title="No search results found"
+                  />
                 ) : (
-                  <RenderCards data={[]} title="No posts found" />
+                  <RenderCards data={allPost} title="No posts found" />
                 )}
               </div>
             </>
