26bf6e90473e9d61f42dce6560df2f13c907d9ac
V-VikramRaja <vvraja2301@gmail.com>
2023-01-28 11:46:32 +0530

    implementing the download function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,6 @@
 // This support function will generate a random number by the number it will take a prompt.
 import { surpriseMePrompts } from "../constants";
+import FileSaver from "file-saver";
 
 export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -7,4 +8,9 @@ export function getRandomPrompt(prompt) {
 
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
   return randomPrompt;
+}
+
+// This function will download the image
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
